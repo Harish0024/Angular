8d72defd0ca0bd8f356ee43395d94984fba0c8d4
Harish0024 <145547051+Harish0024@users.noreply.github.com>
2023-10-30 10:41:53 +0530

    Add files via upload

---
+++ pipes/app/User-Data.ts
@@ -0,0 +1,10 @@
+export class UserData{
+    firstName:string='HARISH';
+    lastName:string='V';
+    location:string='chennai';
+    company:string='GAVS';
+    salary:number=10000;
+    dateofjoining:Date=new Date();
+    
+
+}
